a6478dc282060e8ab59ba830f8cb81dfba65d4bc
Benjamin Jones <bljones36@gmail.com>
2018-11-18 20:29:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -47,9 +47,9 @@ class Application
   def item_search(search_term, resp)
     if @@items.include?(search_term)
       @@cart << search_term
-      resp.write "added #{search_term}"
+      return "added #{search_term}"
     else
-      resp.write "We don't have that item"
+      return "We don't have that item"
     end
   end
 end
