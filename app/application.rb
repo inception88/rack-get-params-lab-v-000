2ac7a3fbbe8b6e9af52da560b4ba7a143db47c80
Benjamin Jones <bljones36@gmail.com>
2018-11-18 20:18:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,9 @@ class Application
     elsif req.path.match(/cart/)
       check_cart(resp)
     elsif req.path.match(/add/)
-      @@cart << 
+      search_term = req.params["q"]
+      if @@items.include?(search_term)
+      @@cart << search_term
       resp.write "added #{item}"
     else
       resp.write "Path Not Found"
@@ -41,6 +43,15 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+    end
+  end
+
+  def item_search(search_term)
+    if @@items.include?(search_term)
+      @@cart << search_term
+      resp.write "added #{search_term}"      
+    else
+      resp.write "We don't have that item"
     end
   end
 end
